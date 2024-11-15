4827b7409488bbe3f61583414be942384ceab5c0
luisarthurlobo <luisarthurlobo@gmail.com>
2024-11-15 19:26:20 +0000

    funfando

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,6 @@
 "use client"
 import AppContainer from '@/components/ui/AppContainer';
 
-interface FormData {
-  // Add your form fields here
-  name: string;
-  email: string;
-  // ... other fields
-}
-
 export default function Page() {
-  const handleSubmit = (data: FormData) => {
-    console.log('Form submitted:', data);
-  };
-
-  return (
-    <AppContainer onSubmit={handleSubmit} />
-  );
+  return <AppContainer />;
 }
